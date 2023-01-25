3579609297396a4f741cfcab00f0385fdf9693e9
Natthanon-b263 <108251256+Natthanon-b263@users.noreply.github.com>
2023-01-25 22:28:07 +0700

    add new files
    add a bunch of new files

---
+++ Fruits/src/entity/Fruit.java
@@ -0,0 +1,41 @@
+package entity;
+public class Fruit {
+    String name;
+    String color;
+    String taste;
+    int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+}
